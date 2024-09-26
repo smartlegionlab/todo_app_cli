93c4f20390b63a52ebdf0fe27900f33f14ffcf59
A.A. Suvorov <smartlegiondev@gmail.com>
2024-09-26 08:44:48 +0700

    Main menu added v0.0.2

--- utils/app_manager.py
+++ utils/app_manager.py
@@ -23,7 +23,42 @@ class AppManager:
         self._printer.show_footer(url=self._config.url, copyright_=self._config.copyright_)
 
     def main_menu(self):
-        print('Welcome to the Cli TODO App!')
+        while 1:
+            self._printer.print_center(text='Main menu:')
+
+            print('1. + Add new task')
+            print('2. Task list')
+            print('0. Exit')
+
+            self._printer.print_center()
+
+            cmd = input('Select the option you want: ')
+
+            if cmd == '1':
+                self.add_task()
+            elif cmd == '2':
+                self.show_task_list()
+            elif cmd == '0':
+                break
+            else:
+                self._show_error()
+
+    def _show_error(self, title='ERROR!!!', text='Error! Invalid input.'):
+        self._printer.print_center(text=title)
+        print(text)
+        self._continue()
+
+    def add_task(self):
+        print('Add new task')
+        self._continue()
+
+    def show_task_list(self):
+        print('Task list')
+        self._continue()
+
+    def _continue(self):
+        self._printer.print_center()
+        input('Press enter to continue... ')
 
     def run(self):
         self.show_head()
