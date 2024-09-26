2a155437232c2e2046b32bdc3ea25bf003dcfd13
A.A. Suvorov <smartlegiondev@gmail.com>
2024-09-26 08:18:55 +0700

    Added configs

---
+++ tools/configs.py
@@ -0,0 +1,15 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2024, A.A. Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+
+
+class Config:
+    name = 'Cli TODO app'
+    url = 'https://github.com/smartlegionlab/'
+    info = 'Copyright © 2024, A.A. Suvorov'
+    help_url = 'https://github.com/smartlegionlab/todo_app_cli/'
